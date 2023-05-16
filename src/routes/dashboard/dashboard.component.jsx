ae201819bdcf1ce6d5d96a8071f0579e611ed9ce
sohostory <sohostory@gmail.com>
2023-05-15 20:08:20 -0500

    added tanstack table

--- src/routes/dashboard/dashboard.component.jsx
+++ src/routes/dashboard/dashboard.component.jsx
@@ -1,7 +1,84 @@
+import React, { useReducer, useState } from "react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import "./dashboard.styles.scss";
+
 const Dashboard = () => {
+  const db = [
+    {
+      id: 1,
+      type: "camera",
+      brand: "Canon",
+      model: "EOS 5D Mark IV",
+      serial: "123456789",
+    },
+    {
+      id: 2,
+      type: "camera",
+      brand: "Canon",
+      model: "EOS 5D Mark IV",
+      serial: "123456789",
+    },
+  ];
+
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    columnHelper.accessor("id", "ID"),
+    columnHelper.accessor("type", "Type"),
+    columnHelper.accessor("brand", "Brand"),
+    columnHelper.accessor("model", "Model"),
+    columnHelper.accessor("serial", "Serial"),
+  ];
+
+  const [data, setData] = useState(() => [...db]);
+  const rerender = useReducer(() => ({}), {})[1];
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="dashboard">
+      <aside className="aside-menu">
+        <h1>aside</h1>
+      </aside>
+      <main className="main-dashboard">
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 };
